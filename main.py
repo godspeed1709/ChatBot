d99ee8fb3300736d45f320d54e58afc2581b7ce1
Srijit Das <61375534+godspeed1709@users.noreply.github.com>
2023-04-16 21:24:31 +0530

    Updated API key

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import customtkinter as ctk
 import os
 import openai
 
-API_KEY = "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"
+API_KEY = "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"
 os.environ['OPENAI_Key'] = API_KEY
 openai.api_key = os.environ['OPENAI_Key']
 
